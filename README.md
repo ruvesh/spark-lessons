5795b1ec897f599c12717f915166fe8b6ffade1a
ruvesh <ruvesh2145@gmail.com>
2021-03-03 23:15:40 +0530

    Input files and SameHosts Problem solution

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spark Lessons
+
+Java Spark Lessons Repository
+
+VCS - git
+<br/>Programming Language - Java
+<br/>Spark Version - 2.1.0
+<br/>Build tool - Maven
---
+++ src/main/java/nasaProblems/SameHosts.java
@@ -0,0 +1,25 @@
+package nasaProblems;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SameHosts {
+    public static void main(String[] args) {
+        /*
+         * find the same hosts that were accessed on both the days logged in nasa apache web log files
+         */
+        SparkConf conf = new SparkConf().setAppName("nasa_same_hosts").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> nasaHostsDay1 = sc.textFile("in/nasa_19950701.tsv")
+                .map(line -> line.split("\t")[0]);
+        JavaRDD<String> nasaHostsDay2 = sc.textFile("in/nasa_19950801.tsv")
+                .map(line -> line.split("\t")[0]);
+
+        JavaRDD<String> sameHosts = nasaHostsDay1.intersection(nasaHostsDay2)
+                .filter(host -> !host.equals("host"));
+
+        sameHosts.saveAsTextFile("out/nasa_logs_same_hosts.csv");
+    }
+}
