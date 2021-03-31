f0a390e80c460cbb5994961682f35eb5a1950566
ruvesh <ruvesh2145@gmail.com>
2021-03-31 12:04:50 +0530

    Group Airport names by country of origin

---
+++ src/main/java/pairRdd/groupbykey/AirportsByCountry.java
@@ -0,0 +1,26 @@
+package pairRdd.groupbykey;
+
+import common.Utils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+
+public class AirportsByCountry {
+    /*
+     *  Group airports in the Airports file by Country
+     */
+
+    public static void main(String[] args) {
+        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("AirportsGroupByCountry").setMaster("local[*]"));
+        JavaRDD<String> lines = sc.textFile("in/airports.text");
+        JavaPairRDD<String, String> airportsAndCountryPairs = lines.mapToPair(
+              airport ->  new Tuple2<>(airport.split(Utils.COMMA_DELIMITER)[3], airport.split(Utils.COMMA_DELIMITER)[1])
+        );
+
+        JavaPairRDD<String, Iterable<String>>  airportsGroupedByCountry = airportsAndCountryPairs.groupByKey();
+        airportsGroupedByCountry.saveAsTextFile("out/GroupedAirports.txt");
+    }
+}
